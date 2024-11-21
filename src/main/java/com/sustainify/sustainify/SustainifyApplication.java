96b0982e6e6c40a2303be67a820e66b0aaa8c650
Sanjana <67232409+koiralasanjana@users.noreply.github.com>
2024-11-21 15:17:05 +0545

    structure

---
+++ src/main/java/com/sustainify/sustainify/SustainifyApplication.java
@@ -0,0 +1,13 @@
+package com.sustainify.sustainify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustainifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SustainifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sustainify/sustainify/SustainifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sustainify.sustainify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustainifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
