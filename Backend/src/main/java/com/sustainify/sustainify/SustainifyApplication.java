0977af66a054228f0d121e5433c4c5775dda7e25
Sanjana <67232409+koiralasanjana@users.noreply.github.com>
2024-11-21 15:54:22 +0545

    backend-setup

--- Backend/src/main/java/com/sustainify/sustainify/SustainifyApplication.java
+++ Backend/src/main/java/com/sustainify/sustainify/SustainifyApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SustainifyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SustainifyApplication.class, args);
 	}
-
 }
