1500ce253af36bb8e8f9378abc39ed261e6807f2
Satyam Khatiwada <satyamkhatiwada10@gmail.com>
2024-12-02 11:37:29 +0545

    # Added some costraints
    # After some changes i have added some constraints in the Reporter modal. and also dependency has been updated.

--- Backend/src/main/java/com/sustainify/sustainify/Model/Reporter.java
+++ Backend/src/main/java/com/sustainify/sustainify/Model/Reporter.java
@@ -2,6 +2,9 @@ package com.sustainify.sustainify.Model;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name= "reporter")
@@ -12,12 +15,15 @@ public class Reporter {
     private Long id;
 
     @Column(nullable = false)
+    @NotEmpty(message = "Name is required")
     private String name;
 
     @Column(nullable = false, unique = true)
+    @Email(message = "Invalid email format")
     private String email;
 
     @Column(nullable = false)
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 
     @Column(name = "created_at", nullable = false, updatable = false)
